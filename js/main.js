03ac1b6c4c8be589dc1ed486b49ca2f9a125a7c6
Laura Garcia <lgarciacuello@gmail.com>
2022-10-31 11:21:33 +0100

    end

--- js/main.js
+++ js/main.js
@@ -2,35 +2,57 @@
 const select = document.querySelector(".js-select");
 const btn = document.querySelector(".js-btn");
 const result = document.querySelector(".js-result");
+const points = document.querySelector(".js-points");
+const reset = document.querySelector(".js-btn-reset");
+
+let countUser = 0;
+let countComputer = 0;
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-const war = () => {
-  const number = getRandomNumber(5);
-  //   if (selectValue === number) {
-  //     result.innerHTML = "Empate";
-  //   } else if (selectValue < number) {
-  //     result.innerHTML = "Ha ganado el Ejército del Mal! Vuelve a Intentarlo.";
-  //   } else {
-  //     result.innerHTML = "Ha ganado el Ejército del Bien! Enhorabuena.";
-  //   }
+function badRace() {
+  const race = getRandomNumber(5);
 
-  console.log(number);
+  if (race === 1) {
+    raceStrength = 2;
+  } else if (race === 2) {
+    raceStrength = 2;
+  } else if (race === 3) {
+    raceStrength = 2;
+  } else if (race === 4) {
+    raceStrength = 3;
+  } else if (race === 5) {
+    raceStrength = 4;
+  }
+  return raceStrength;
+}
+
+const war = () => {
   const selectValue = select.value;
-  if (selectValue === number) {
+  if (selectValue === raceStrength) {
     result.innerHTML = "Empate";
-  } else if (selectValue < number) {
+  } else if (selectValue < raceStrength) {
     result.innerHTML = "Ha ganado el Ejército del Mal! Vuelve a Intentarlo.";
+    countComputer++;
   } else {
     result.innerHTML = "Ha ganado el Ejército del Bien! Enhorabuena.";
+    countUser++;
   }
 };
 
+function paintHTML(countUser, countComputer) {
+  points.innerHTML = `<li>Usuario: ${countUser} - máquina: ${countComputer} </li>`;
+}
+
 function handleClickWar(ev) {
   ev.preventDefault();
-  console.log("comienza la guerra");
   war();
+  paintHTML(countUser, countComputer);
+}
+function gameOver() {
+  if (count === 10) {
+  }
 }
 
 btn.addEventListener("click", handleClickWar);

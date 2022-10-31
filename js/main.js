87f1e0fc164e31893c428d2ce6ade3cd1e0335e5
Laura Garcia <lgarciacuello@gmail.com>
2022-10-31 09:40:31 +0100

    check js

--- js/main.js
+++ js/main.js
@@ -6,10 +6,27 @@ const result = document.querySelector(".js-result");
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
+const war = () => {
+  const number = getRandomNumber(5);
+  console.log(number);
+  const selectValue = select.value;
+
+  if (selectValue === number) {
+    result.innerHTML("Empate");
+  } 
+  else if (selectValue < number) {
+    result.innerHTML("Ha ganado el Ejército del Mal! Vuelve a Intentarlo.");
+  }
+  else (select > number) {
+    result.innerHTML("Ha ganado el Ejército del Bien! Enhorabuena.");
+
+  }
+};
 
 function handleClickWar(ev) {
   ev.preventDefault();
   console.log("comienza la guerra");
+  war();
 }
 
 btn.addEventListener("click", handleClickWar);
